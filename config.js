7338096efdbd809fccb7232e01486e4f73d59b09
tannguyen2207 <85267136+tannguyen2207@users.noreply.github.com>
2021-06-03 09:36:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'EM à!',
+    introTitle: 'Gửi tới Hồng Phúc... Công chúa của anh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
